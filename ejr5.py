5d91d6b970817c4eb44efba3c903e61621816805
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-10-07 16:05:00 +0200

    ejr5

---
+++ ejr5.py
@@ -0,0 +1,46 @@
+class Vehiculo():
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+    
+    def __str__(self):
+        return "color {}, {} ruedas".format( self.color, self.ruedas )
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
+
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+    def __str__(self):
+        return Coche.__str__(self) + ", {}Kg".format(self.carga)
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        Vehiculo.__init__(self, color, ruedas)
+        self.tipo = tipo
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {}".format(self.tipo)
+
+class Motocicleta(Bicicleta):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        Bicicleta.__init__(self, color, ruedas, tipo)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return Bicicleta.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
+
+c = Coche("azul", 4, 150, 1200)
+ca = Camioneta("negro", 4, 120, 1000, 1200)
+b = Bicicleta("blanco", 2, "deportiva")
+m = Motocicleta("rojo", 2, "urbana", 160, 500)
+print(c)
+print(ca)
+print(b)
+print(m)
