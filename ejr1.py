48d221f38001473afbc5b95e8e155971c712cf18
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-10-07 15:44:40 +0200

    experimental

--- ejr1.py
+++ ejr1.py
@@ -8,3 +8,17 @@ class alumno():
             print("El alumno", self.nombre, "ha aprobado")
         else:
             print("El alumno", self.nombre, "ha suspendido")
+
+German = alumno("German", 0)
+Carlos = alumno("Carlos", "5")
+Zazo = alumno("Zazo", 2)
+Migui = alumno("Migui", 0.5)
+Moyis = alumno("Moyis", 10)
+Gonzalo = alumno("Gonzalo", 6.1)
+
+German.calificacion()
+Carlos.calificacion()
+Zazo.calificacion()
+Migui.calificacion()
+Moyis.calificacion()
+Gonzalo.calificacion()
